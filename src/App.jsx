5927178ffcc9cb26dbe030dd78b263cae3eab98e
Emiro-Osp <120336414+Emiro-Osp@users.noreply.github.com>
2023-03-13 21:40:36 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { MoviesGrid } from "./components/MoviesGrid";
+import styles from './components/App.module.css';
+import { BrowserRouter as Router, Switch, Route, Link
+} from "react-router-dom";
+import { MovieDetails } from "./pages/MoviesDetails";
+import { LandingPages } from "./pages/LandingPages";
+
+export function App(){
+    return  (
+    <Router>
+        <header>
+        <Link to="/">  
+        <h1 className={styles.title}> Movies </h1> 
+        </Link>
+        </header>
+        
+        <main>
+        <Switch>
+          <Route exact path="/movies/:movieId"><MovieDetails /> 
+          </Route>
+          <Route path="/"><LandingPages /> 
+          </Route>
+        </Switch>
+        </main>
+    </Router>
+    
+    );
+}
